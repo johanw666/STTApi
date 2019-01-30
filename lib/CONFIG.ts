f646af8402e950feddef39e68d2b74db17bde993
Unknown <johanw@xs4all.nl>
2019-01-30 11:23:57 +0100

    Updated client version number.

--- lib/CONFIG.ts
+++ lib/CONFIG.ts
@@ -19,7 +19,7 @@ export default class CONFIG {
 	// default client_id of the Steam Windows version of STT
 	static readonly CLIENT_ID: string = "4fc852d7-d602-476a-a292-d243022a475d";
 	static readonly CLIENT_API_VERSION: number = 12;
-	static readonly CLIENT_VERSION: string = "6.0.0";
+	static readonly CLIENT_VERSION: string = "7.0.9";
 	static readonly CLIENT_PLATFORM: string = "webgl";
 
 	// releases URL
